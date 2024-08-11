b0f8fd6a60911848bbdf7559cb2f3eacfe0201e0
Excilious <120273588+Excilious@users.noreply.github.com>
2024-08-11 17:14:03 +0100

    Update Simon Says C++.ino

--- Simon Says C++.ino
+++ Simon Says C++.ino
@@ -1,9 +1,24 @@
+/*
+  Copyright (C) 2021 Excilious
+  
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+  
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <Adafruit_LiquidCrystal.h>
 #include <Servo.h>
 
 Adafruit_LiquidCrystal lcd_1(0);
-
-
 
 //Level system + speed
 const int MAX_LEVEL = 100;
